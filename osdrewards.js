437b9dc72914cdd825bf9715cb91bee4140f4e29
Chris Angelico <rosuav@gmail.com>
2020-03-21 08:45:53 +1100

    Add a channel name form

--- osdrewards.js
+++ osdrewards.js
@@ -1,10 +1,19 @@
 import choc, {set_content} from "https://rosuav.github.io/shed/chocfactory.js";
 import "https://cdn.jsdelivr.net/npm/comfy.js/dist/comfy.min.js"; const ComfyJS = window.ComfyJS;
-const {A, LI} = choc;
+const {A, FORM, INPUT, LI} = choc;
 
 const params = new URLSearchParams(window.location.search);
+let channel = params.get("channel");
+if (!channel) {
+	document.getElementById("people").appendChild(LI(FORM([
+		"Enter your channel name: ",
+		INPUT({name: "channel"}),
+		INPUT({type: "submit", value: "Go!"}),
+	])));
+}
+
 let rewardid = params.get("rewardid");
-if (!rewardid) {
+if (channel && !rewardid) {
 	document.getElementById("people").appendChild(LI(
 		"As people redeem your custom reward, they'll appear here. " +
 		"You'll need to pick a specific reward - try it out and you'll get a link."
@@ -16,7 +25,7 @@ ComfyJS.onChat = ( user, message, flags, self, extra ) => {
 	if (!rewardid) {
 		document.getElementById("people").appendChild(LI([
 			"Custom reward sighted with message " + message + " ",
-			A({href: window.location + "?rewardid=" + extra.customRewardId},
+			A({href: window.location + "&rewardid=" + extra.customRewardId},
 				"Add THIS link as a browser source to watch for this reward."
 			),
 		]));
@@ -28,5 +37,4 @@ ComfyJS.onChat = ( user, message, flags, self, extra ) => {
 
 //TODO: Mod-only command to initiate and clear things
 
-const username = "Rosuav"; //TODO: Get this from a query param
-ComfyJS.Init(username);
+if (channel) ComfyJS.Init(channel);
